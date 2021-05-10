c78f328c498b05ebbb1fc4ab500b0baa6caae542
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-05-10 16:33:30 +0300

    Delete task-01.js

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("#categories .item").forEach(cat => {
-  const title = cat.querySelector("h2").innerHTML;
-  const liElements = cat.querySelectorAll("li");
-  console.log(`Категория: ${title}`);
-  console.log(`количество элементов: ${liElements.length}`);
-});
-
